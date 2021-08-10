b5cfae8adac68a609d0e2d7af6e1cd68a0bca743
karivijaya <59792681+karivijaya@users.noreply.github.com>
2021-08-10 10:42:06 +0530

    Add files via upload

---
+++ ExpressoApi/Controllers/MenusController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpressoApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExpressoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+   
+    public class MenusController : ControllerBase
+    {
+        ExpressoDbContext expressoDbContext;
+
+        public MenusController(ExpressoDbContext _expressoDbContext)
+        {
+            expressoDbContext = _expressoDbContext;
+        }
+
+        [HttpGet]
+      
+        public IActionResult GetMenus()
+        {
+            var menus = expressoDbContext.Menus.Include("SubMenus");
+            return Ok(menus);
+        }
+
+        [HttpGet("{id}")]
+        
+        public IActionResult GetMenu(int id)
+        {
+            var menu = expressoDbContext.Menus.Include("SubMenus").FirstOrDefault(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menu);
+        }
+    }
+}
---
+++ ExpressoApi/Controllers/ReservationsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpressoApi.Data;
+using ExpressoApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExpressoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class ReservationsController : ControllerBase
+    {
+        private ExpressoDbContext expressoDbContext;
+
+        public ReservationsController(ExpressoDbContext _expressoDbContext)
+        {
+            expressoDbContext = _expressoDbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Reservation reservation)
+        {
+            expressoDbContext.Reservations.Add(reservation);
+            expressoDbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
